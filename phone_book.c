27ffa5c4a934d0a09058626d9bbf199dbff65ea7
geetha106 <84716877+geetha106@users.noreply.github.com>
2021-07-08 12:57:57 +0530

    Update phone_book.c

--- phone_book.c
+++ phone_book.c
@@ -21,11 +21,11 @@ int delete(FILE *, char *);
 /* Utility functions  */
 FILE * open_db_file(); /* Opens the database file. Prints error and
                           quits if it's not available */
-void print_usage(char *, char *);  /* Prints usage */
-entry *load_entries(FILE *);         /* Load all entries from the
+void print_usage(char , char *);  /* Prints usage */
+entry load_entries(FILE *);         /* Load all entries from the
                                       database file. Returns pointer
                                       to first entry */
-entry *create_entry_node(char *, char *);  /* Create a new entry
+entry create_entry_node(char *, char *);  /* Create a new entry
                                               node. Has to be freed by
                                               user. */
 void free_entries(entry *); /* TBD Given the first node of a linked list
@@ -191,7 +191,7 @@ int delete(FILE *db_file, char *name) {
   entry *p = load_entries(db_file);
   entry *base = p;
   entry *prev = NULL;
-  entry *del = NULL ; /* Node to be deleted */
+  entry del = NULL ; /* Node to be deleted */
   int deleted = 0;
   while (p!=NULL) {
     if (strcmp(p->name, name) == 0) {
